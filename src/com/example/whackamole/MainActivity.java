204d3e22575b69bc855606b6f1dc86093c076075
Pierre Bourré <pierre.bourre@telecom-sudparis.eu>
2014-12-09 22:52:26 +0100

    Premier commit, image qui s'affiche en arrière plan, premier point de l'exo: done

---
+++ src/com/example/whackamole/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.whackamole;
+
+import java.io.File;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+	private Uri fileUri;
+	View mView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+			mView = new View(this);
+			setContentView(mView);
+
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+			File imageFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			File image = new File(imageFolder, "CapturedImage.jpg" );
+			fileUri = Uri.fromFile(image);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+			// start the image capture Intent
+			startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				Options options = new BitmapFactory.Options();
+				options.inScaled = false;
+				options.inDither = false;
+				options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+				Bitmap photo = BitmapFactory.decodeFile(fileUri.getPath(), options);
+
+				mView.setBackground(new BitmapDrawable(getResources(), photo));
+
+			} else if (resultCode == RESULT_CANCELED) {
+				// User cancelled the image capture
+			} else {
+				// Image capture failed, advise user
+			}
+
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
